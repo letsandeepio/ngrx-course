32a0ca46d112fb2181a5de48c3d9578498e16818
Sandeep Chopra <sandeepchopra7@gmail.com>
2023-04-21 14:20:45 -0400

    add the load all courses reducer

---
+++ src/app/courses/reducers/course.reducers.ts
@@ -0,0 +1,17 @@
+import { EntityState, createEntityAdapter } from "@ngrx/entity";
+import { Course } from "../model/course";
+import { createReducer, on } from "@ngrx/store";
+import { CourseActions } from "../action-types";
+
+export interface CoursesState extends EntityState<Course> {}
+
+export const adapter = createEntityAdapter<Course>();
+
+export const initialCoursesState = adapter.getInitialState();
+
+export const coursesReducer = createReducer(
+  initialCoursesState,
+  on(CourseActions.allCoursesLoaded, (state, action) =>
+    adapter.setAll(action.courses, state)
+  )
+);
