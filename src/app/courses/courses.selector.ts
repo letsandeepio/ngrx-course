2b0ceb53906ed4c495458ed4bf1ddfa48c65c684
Sandeep Chopra <sandeepchopra7@gmail.com>
2023-04-24 14:45:01 -0400

    refactor home component to use selectors from store

---
+++ src/app/courses/courses.selector.ts
@@ -0,0 +1,27 @@
+import { createFeatureSelector, createSelector } from "@ngrx/store";
+import { CoursesState } from "./reducers/course.reducers";
+
+import * as fromCourses from "./reducers/course.reducers";
+
+export const selectCoursesState =
+  createFeatureSelector<CoursesState>("courses");
+
+export const selectAllCourse = createSelector(
+  selectCoursesState,
+  fromCourses.selectAll
+);
+
+export const selectBeginnerCourses = createSelector(
+  selectAllCourse,
+  (courses) => courses.filter((course) => course.category === "BEGINNER")
+);
+
+export const selectAdvancedCourses = createSelector(
+  selectAllCourse,
+  (courses) => courses.filter((course) => course.category === "ADVANCED")
+);
+
+export const selectPromotTotal = createSelector(
+  selectAllCourse,
+  (courses) => courses.filter((course) => course.promo).length
+);
